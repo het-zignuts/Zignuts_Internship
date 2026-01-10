7e255499690c2bf00e3db689310369ccad5ecd7a
het-zignuts <hets@zignuts.com>
2026-01-10 17:44:23 +0530

    Delete authentication-authorization directory

--- authentication-authorization/app/db/init_db.py
+++
@@ -1,6 +0,0 @@
-from sqlmodel import SQLModel
-from app.models import user, book
-from app.db.session import db_session_manager
-
-def init_db():
-    SQLModel.metadata.create_all(db_session_manager.engine)
