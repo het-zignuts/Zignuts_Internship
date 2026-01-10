4ef37fd2b5912a97c522e66d966619272474856b
het-zignuts <hets@zignuts.com>
2026-01-10 17:48:38 +0530

    Delete books_collect_persistent_crud directory

--- books_collect_persistent_crud/app/db/session.py
+++
@@ -1,9 +0,0 @@
-from sqlmodel import SQLModel, Session, create_engine
-
-DATABASE_URL = "sqlite:///./books.db"
-
-engine=create_engine(DATABASE_URL, echo=True)
-
-def get_session():
-    with Session(engine) as session:
-        yield session
