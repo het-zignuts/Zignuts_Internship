9df701afd6090ac0e9d74896e90eb04bacbf324e
het-zignuts <hets@zignuts.com>
2026-01-10 17:46:20 +0530

    Delete logging_middleware directory

--- logging_middleware/app/db/books_db.py
+++
@@ -1,9 +0,0 @@
-from sqlmodel import SQLModel, Field
-from uuid import UUID, uuid4
-
-class Book(SQLModel, table=True):
-    id: UUID = Field(default_factory=uuid4, primary_key=True)
-    title: str = Field(max_length=200)
-    author: str = Field(max_length=100)
-    isbn: str = Field(max_length=13, unique=True)
-    publication_year: int = Field(ge=0)
